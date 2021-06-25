40f4c37e46f3e3c312cc27f03ccf6c2647a5f8bc
MIGUELC-PHEJ9IR\mconchouso <mconchouso@gmail.com>
2021-06-25 10:55:39 +0200

    Test Stage

---
+++ MyFirstAppTests/CalculatorTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyFirstApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp.Tests
+{
+    [TestClass()]
+    public class CalculatorTests 
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Calculator calc = new Calculator();
+            Assert.AreEqual(5, calc.Add(3, 2));
+        }
+
+        [TestMethod()]
+        public void SubtractTest()
+        {
+            Calculator calc = new Calculator();
+            Assert.AreEqual(1, calc.Subtract(3, 2));
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            Calculator calc = new Calculator();
+            Assert.AreEqual(6, calc.Multiply(3, 2));
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            Calculator calc = new Calculator();
+            Assert.AreEqual(3, calc.Divide(6, 2));
+        }
+    }
+}
--- MyFirstAppTests/Properties/AssemblyInfo.cs
+++ MyFirstAppTests/Properties/AssemblyInfo.cs
@@ -5,32 +5,32 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("MyFirstApp")]
+[assembly: AssemblyTitle("MyFirstAppTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MyFirstApp")]
+[assembly: AssemblyProduct("MyFirstAppTests")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
-// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// a los componentes COM. Si necesita obtener acceso a un tipo en este ensamblado desde 
 // COM, establezca el atributo ComVisible en true en este tipo.
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("dc8833fa-a1db-4906-96b2-d637079c6275")]
+[assembly: Guid("bdadc4d0-776b-489c-9ce2-6ca4905bad54")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
 //      Versión principal
-//      Versión secundaria
+//      Versión secundaria 
 //      Número de compilación
 //      Revisión
 //
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
-// [assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
